12f8b06149689b46e295fb481a4dee72de5c9bb8
jovenesipup <jovenes.ipup@gmail.com>
2022-10-01 16:11:07 -0500

    asd

--- src/pages/registro.jsx
+++ src/pages/registro.jsx
@@ -7,7 +7,7 @@ export default function registro() {
     <div className="container backdrop-blur-3xl mx-auto lg:px-16">
       <div className="py-36 lg:flex lg:justify-between ">
         <div className="lg:max-w-lg">
-          <h1 className="text-slate-50 text-4xl font-bold lg:text-5xl">Muy bien! Estas muy cera</h1>
+          <h1 className="text-slate-50 text-4xl font-bold lg:text-5xl">Muy bien! Estas muy cerca</h1>
           <p className="text-slate-50 text-xl py-8 lg:font-bold lg:text-2xl">Solo tienes que rellenar el formulario y seguir los pasos.</p>
           <img src={Joven} alt="" className="hidden lg:block lg:max-w-lg h-auto  lg:rounded-lg lg:shadow-lg lg:shadow-light-green lg:border-4 lg:border-x-light-purple lg:border-y-light-green"/>
         </div>
