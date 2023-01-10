9f4292f31a1fdf8ce446b002b3e37dd7efc68e10
jovenesipup <jovenes.ipup@gmail.com>
2023-01-10 10:55:35 -0500

    dsgfd

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -45,7 +45,9 @@ export default function form() {
       ninos: formEl.querySelector("#user_ninos").value,
       civil: formEl.querySelector("#user_civil").value,
       talla: formEl.querySelector("#user_talla").value,
-      genero: formEl.querySelector("#user_genero").value
+      genero: formEl.querySelector("#user_genero").value,
+      infante: formEl.querySelector("#user_infante").value,
+      edad: formEl.querySelector("#user_edad").value
     };
     const res = await axios
       .post("https://campamentoapi.pro/api", {
@@ -61,7 +63,9 @@ export default function form() {
         ninos: formBody.ninos || 0,
         civil: formBody.civil,
         talla: formBody.talla,
-        genero: formBody.genero
+        genero: formBody.genero,
+        infante: formBody.infante,
+        edad: formBody.edad
       })
       .then(function (response) {
         sendEmail();
@@ -147,6 +151,37 @@ export default function form() {
             maxLength={9}
             name="user_number"
           />
+          <Input
+            labelName="Edad"
+            placeholder="edad"
+            type="number"
+            isRequired="required"
+            maxLength={9}
+            name="user_edad"
+          />
+          <div className="mb-6">
+            <label
+              htmlFor="price"
+              className="block text-lg font-medium text-slate-50"
+            >
+              Soy un niño
+            </label>
+            <div className="mt-1 relative rounded-md shadow-sm">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <span className="text-gray-500 sm:text-sm"></span>
+              </div>
+              <select
+                className="block w-full pl-4 pr-12 py-2 rounded-2xl text-lg bg-dark-lila/60 text-slate-50 placeholder:text-slate-600"
+                required
+                name="user_infante"
+                id="user_infante"
+              >
+                <option value="">Seleccione</option>
+                <option value="true">Si</option>
+                <option value="false">No</option>
+              </select>
+            </div>
+          </div>
           <div className="mb-6">
             <label
               htmlFor="price"
@@ -222,14 +257,6 @@ export default function form() {
                   id="user_talla"
                 >
                   <option value="">Seleccione</option>
-                  <option value="2">2</option>
-                  <option value="4">4</option>
-                  <option value="6">6</option>
-                  <option value="8">8</option>
-                  <option value="10">10</option>
-                  <option value="12">12</option>
-                  <option value="14">14</option>
-                  <option value="16">16</option>
                   <option value="S">S</option>
                   <option value="M">M</option>
                   <option value="L">L</option>
